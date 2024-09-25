f771fdc5cd2a0a043eae843a37adad0ede571c9f
daik1i-man <olimboevjoshqin@gmail.com>
2024-09-26 01:43:11 +0500

    Fixed connection bugs with server of the client project

--- server/Database/config.js
+++ server/Database/config.js
@@ -1,8 +1,7 @@
 const { Pool } = require('pg')
-const DATABASE_URL = 'postgresql://postgres:pCtItvJgjisuLyvqLRaNcfdQcVBCApjn@junction.proxy.rlwy.net:22922/railway'
 
 const pool = new Pool({
-    connectionString: DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 })
 
 module.exports = pool;
