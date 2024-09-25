07be1a3bdcf877c1a4a42765432ccda13a0dde61
daik1i-man <olimboevjoshqin@gmail.com>
2024-09-25 05:01:55 +0500

    Chaged database url and fixed admin panel bugs

--- server/Database/config.js
+++ server/Database/config.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg')
-const DATABASE_URL = 'postgresql://uwzdgesqjr03dg0g3v55:6wcRtF8CpHfNSl7JLWNn@b09urzatr9jcssfmufl5-postgresql.services.clever-cloud.com:5237/b09urzatr9jcssfmufl5'
+const DATABASE_URL = 'postgresql://postgres:pCtItvJgjisuLyvqLRaNcfdQcVBCApjn@junction.proxy.rlwy.net:22922/railway'
 
 const pool = new Pool({
     connectionString: DATABASE_URL,
